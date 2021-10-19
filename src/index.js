10d2c3273b24458104886584ad72d6cfe1c39d6d
Hayden Schroeder <hschroeder0317@gmail.com>
2021-10-18 22:00:21 -0500

    fixed, on 'lifting state up'

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square"
-                onClick={() => { this.props.onClick({ value: 'X' }); }}>
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
+            >
                 {this.props.value}
             </button>
         );
@@ -14,11 +16,17 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
     renderSquare(i) {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={() => { this.handleClick(i) }}
+                onClick={() => this.handleClick(i)}
             />
         );
     }
@@ -50,12 +58,6 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        };
-    }
     render() {
         return (
             <div className="game">
